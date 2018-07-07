c4928f43288423073fccf8a6d2b2c1b6f588cf56
Cap'n_Rob <robb1772@hotmail.com>
2018-07-07 14:30:14 -0700

    secondsave

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { BlogsComponent } from './components/blogs/blogs.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
 
 const routes: Routes = [];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { BlogsComponent } from './components/blogs/blogs.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BlogsComponent,
+    ContactsComponent
   ],
   imports: [
     BrowserModule,
